2817590194b68ac68bfde3af6a6011a4cd671220
liwenbin <837531387@qq.com>
2017-09-08 08:42:04 +0800

    上传图片

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-var utils = require('../commons/utils');
+var path = require('path');
 
+var utils = require('../commons/utils');
 var sendData = utils.sendData;
 
-/* GET home page. */
+/* 获取图片列表 */
 router.get('/file/getImageList', function (req, res, next) {
     var fs = require('fs');
-    var path = require('path');
+
     var picPath = path.join(__dirname + '/../public/pic');
     var picdir = fs.readdir(picPath, function (err, files) {
         if (err) {
@@ -19,6 +20,29 @@ router.get('/file/getImageList', function (req, res, next) {
         }
     })
 
+});
+
+
+/*上传图片*/
+router.post('/file/uploadImage', function (req, res, next) {
+    console.log(req);
+    var multiparty = require('multiparty');
+    var fs = require('fs');
+
+    //生成multiparty对象，并配置上传目标路径
+    var form = new multiparty.Form({uploadDir: './public/pic/'});
+    //上传完成后处理
+    form.parse(req, function (err, fields, files) {
+        var filesTmp = JSON.stringify(files, null, 2);
+
+        if (err) {
+            res.send(sendData('999', err, '上传错误'));
+        } else {
+            var name = files.pic[0].path;
+            res.send(sendData('200', path.basename(name), ''));
+        }
+
+    });
 });
 
 
